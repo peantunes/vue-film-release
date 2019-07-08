b0fef268fe4e6e7c8d9af7f7c815e7ffa88043ef
Pedro Antunes <peantunes@gmail.com>
2019-07-08 23:10:09 +0100

    fixing behaviour that was showing the previous selected product

--- src/store/modules/films.js
+++ src/store/modules/films.js
@@ -30,10 +30,10 @@ const actions = {
     commit("setGenres", response.data.genres);
   },
   async filmDetail({ commit }, id) {
+    commit("setCurrentFilm", null);
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${id}?api_key=d4b2ef2c0b83491e9846e0c0f9e0bbe7`
     );
-    console.log(response.data);
     commit("setCurrentFilm", response.data);
   }
 };
